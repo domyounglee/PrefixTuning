f5b3d94e1d557128b38057948dd9c36f47f92e07
Joseph <j16065682@gmail.com>
2023-11-24 13:20:11 +0900

    update path

--- gpt2/run_generation.py
+++ gpt2/run_generation.py
@@ -814,16 +814,16 @@ def main():
             prompt_text_lst = list(prompt_text_dict.keys())
             split_file = args.eval_dataset
             decode_mode = 'beam'
-            curr_dir = os.path.join('/content/text-generation/',
+            curr_dir = os.path.join('/home/hanati/hit/',
                                     args.gen_dir,
                                     '{}_{}_{}'.format(temp, split_file, decode_mode))
             print(curr_dir)
-            gold_dir = os.path.join('/content/text-generation/',
+            gold_dir = os.path.join('/home/hanati/hit/',
                                     args.gen_dir,
                                     '{}_{}_{}'.format(temp, split_file,'gold'))
             print(gold_dir)
             write_e2e_corr(prompt_text_lst, prompt_text_dict, gold_dir)
-            src_dir = os.path.join('content/text-generation/',
+            src_dir = os.path.join('/home/hanati/hit/',
                                    args.gen_dir,
                                    '{}_{}_{}'.format(temp,split_file, 'src'))
             write_e2e_src(prompt_text_lst, src_dir)
@@ -859,16 +859,16 @@ def main():
                 temp = os.path.basename(args.model_name_or_path)
             split_file = args.eval_dataset # test
             decode_mode = 'beam'
-            curr_dir = os.path.join('/content/text-generation/',
+            curr_dir = os.path.join('/home/hanati/hit/',
                                     args.gen_dir,
                                     '{}_{}_{}'.format(temp, split_file, decode_mode))
             print(curr_dir)
-            gold_dir = os.path.join('/content/text-generation/',
+            gold_dir = os.path.join('/home/hanati/hit/',
                                     args.gen_dir,
                                     '{}_{}_{}'.format(temp, split_file, 'gold'))
             print(gold_dir)
             write_e2e_corr(prompt_text_pair, prompt_text_dict, gold_dir)
-            src_dir = os.path.join('/content/text-generation/',
+            src_dir = os.path.join('/home/hanati/hit/',
                                     args.gen_dir,
                                     '{}_{}_{}'.format(temp, split_file, 'src'))
             write_e2e_src(prompt_text_pair, src_dir)
@@ -904,13 +904,13 @@ def main():
             # print(prompt_text_dict)
             split_file = 'test' # test
             decode_mode = 'greedy'
-            curr_dir = os.path.join('/content/text-generation/',
+            curr_dir = os.path.join('/home/hanati/hit/',
                                     args.gen_dir,
                                     '{}_{}_{}'.format(temp, split_file, decode_mode))
             # curr_dir = '/content/text-generation/classify_results/{}_{}_{}'.format(
             #     temp, split_file, decode_mode)
             print(curr_dir)
-            gold_dir = os.path.join('/content/text-generation/',
+            gold_dir = os.path.join('/home/hanati/hit/',
                                     args.gen_dir,
                                     '{}_{}_{}'.format(temp, split_file, 'gold'))
             # gold_dir = '/content/text-generation/classify_results/{}_{}_{}'.format(
@@ -919,7 +919,7 @@ def main():
             #     'gold')
             print(gold_dir)
             write_e2e_src(prompt_text_tgt, gold_dir)
-            src_dir = os.path.join('/content/text-generation/',
+            src_dir = os.path.join('/home/hanati/hit/',
                                    args.gen_dir,
                                    '{}_{}_{}'.format(temp, split_file, 'src'))
             # src_dir = '/content/text-generation/classify_results/{}_{}_{}'.format(
@@ -958,13 +958,13 @@ def main():
             # print(prompt_text_dict)
             split_file = 'test' # test
             decode_mode = 'beam'
-            curr_dir = os.path.join('/content/text-generation/',
+            curr_dir = os.path.join('/home/hanati/hit/',
                                     args.gen_dir,
                                     '{}_{}_{}'.format(temp, split_file, decode_mode))
 
 
             print(curr_dir)
-            gold_dir = os.path.join('/content/text-generation/',
+            gold_dir = os.path.join('/home/hanati/hit/',
                                     args.gen_dir,
                                     '{}_{}_{}'.format(temp, split_file, 'gold'))
             print(gold_dir)
